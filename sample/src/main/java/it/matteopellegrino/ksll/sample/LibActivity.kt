83d0802c4d9a14d440cb2215a572e9cefac4443b
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2018-04-27 18:29:17 +0200

    Add LibActivity for details about a specific library

---
+++ sample/src/main/java/it/matteopellegrino/ksll/sample/LibActivity.kt
@@ -0,0 +1,12 @@
+package it.matteopellegrino.ksll.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LibActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lib)
+    }
+}
--- sample/src/main/java/it/matteopellegrino/ksll/sample/adapter/LibAdapter.kt
+++ sample/src/main/java/it/matteopellegrino/ksll/sample/adapter/LibAdapter.kt
@@ -1,18 +1,20 @@
 package it.matteopellegrino.ksll.sample.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import it.matteopellegrino.ksll.model.Lib
+import it.matteopellegrino.ksll.sample.LibActivity
 import it.matteopellegrino.ksll.sample.R
 import kotlinx.android.synthetic.main.item_lib.view.*
 
 
 /**
- * TODO: Add class description
+ * Adapter for a collection of [Lib]
  *
  * @author Matteo Pellegrino matteo.pelle.pellegrino@gmail.com
  */
@@ -23,10 +25,21 @@ class LibAdapter( val context: Context, val objects: List<Lib>): RecyclerView.Ad
         val extension: TextView = view.textExtension
     }
 
+    fun <T : RecyclerView.ViewHolder> T.listen(event: (position: Int, type: Int) -> Unit): T {
+        itemView.setOnClickListener {
+            event.invoke(adapterPosition, itemViewType)
+        }
+        return this
+    }
+
     override fun getItemCount(): Int = objects.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-            ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_lib, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val vh = ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_lib, parent, false))
+        return vh.listen { position, type ->
+            context.startActivity(Intent(context, LibActivity::class.java))
+        }
+    }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         var myholder = holder as ViewHolder
